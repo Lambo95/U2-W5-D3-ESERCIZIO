acbbb6661e8fc57509c06e8966151a18232011bf
Lambo95 <lamberto.martini95@gmail.com>
2022-09-28 18:20:32 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 function start() {
-  document.getElementsById("content").style.transformOrigin = "center";
   document.getElementsById("content").style.transformStyle = "preserve-3d";
-  document.getElementsById("content").style.transform = "translateZ(-30vw) rotateY(0)";
   document.getElementsById("content").style.animation = "carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
 }
 //     document.getElementsById("content").style = "none";
