556f38a64118a0b4a4f2d2c0e88b229b7dab0cb9
Lambo95 <lamberto.martini95@gmail.com>
2022-09-28 16:58:50 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 let start = document.getElementById("start");
-let stop = document.getElementById("stop");
 
 function startAnimation() {
-  document.getElementsById("content").style = "transform-origin: center";
-  document.getElementsById("content").style = "  transform-style: preserve-3d";
-  document.getElementsById("content").style = "transform: translateZ(-30vw) rotateY(0)";
-  document.getElementsById("content").style = "animation: carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
+  if (start) {
+    document.getElementsById("content").style = "transform-origin: center";
+    document.getElementsById("content").style = "  transform-style: preserve-3d";
+    document.getElementsById("content").style = "transform: translateZ(-30vw) rotateY(0)";
+    document.getElementsById("content").style = "animation: carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
+  } else {
+    document.getElementsById("content").style = "none";
+    document.getElementsById("content").style = "none";
+    document.getElementsById("content").style = "none";
+    document.getElementsById("content").style = "none";
+  }
 }
