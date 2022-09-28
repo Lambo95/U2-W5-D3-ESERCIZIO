a35ac32dd013fa16b65d6d5ac76b151fe9378385
Lambo95 <lamberto.martini95@gmail.com>
2022-09-28 17:21:28 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
-let start = document.getElementById("start");
-
-function startAnimation() {
-  if (start) {
-    document.getElementsById("content").style = "transform-origin: center";
-    document.getElementsById("content").style = "  transform-style: preserve-3d";
-    document.getElementsById("content").style = "transform: translateZ(-30vw) rotateY(0)";
-    document.getElementsById("content").style = "animation: carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
-  } else {
-    document.getElementsById("content").style = "none";
-    document.getElementsById("content").style = "none";
-    document.getElementsById("content").style = "none";
-    document.getElementsById("content").style = "none";
-  }
+function start() {
+  document.getElementsById("content").style.transformOrigin = "center";
+  document.getElementsById("content").style.transformStyle = "preserve-3d";
+  document.getElementsById("content").style.transform = "translateZ(-30vw) rotateY(0)";
+  document.getElementsById("content").style.animation = "carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
 }
+//     document.getElementsById("content").style = "none";
+//     document.getElementsById("content").style = "none";
+//     document.getElementsById("content").style = "none";
+//     document.getElementsById("content").style = "none";
+
+// }
