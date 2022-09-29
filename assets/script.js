180284dbd34f264e0c9d99b567ae17855628271a
Lambo95 <lamberto.martini95@gmail.com>
2022-09-29 16:33:06 +0200

    update

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,17 @@
+let animated = document.querySelector("#animated");
+
 function start() {
-  document.getElementsById("content").style.transformStyle = "preserve-3d";
-  document.getElementsById("content").style.animation = "carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
+  animated.classList.remove("stop");
 }
+
+function stop() {
+  animated.classList.add("stop");
+}
+
+// function start() {
+//   document.getElementsById("content").style.transformStyle = "preserve-3d";
+//   document.getElementsById("content").style.animation = "carousel 10s infinite cubic-bezier(0.77, 0, 0.175, 1) forwards";
+// }
 //     document.getElementsById("content").style = "none";
 //     document.getElementsById("content").style = "none";
 //     document.getElementsById("content").style = "none";
